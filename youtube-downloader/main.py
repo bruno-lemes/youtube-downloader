87a74b640a853cb4fff2ab33f3f675b216f03c55
Bruno Lemes <bruno29batista@gmail.com>
2022-12-15 18:48:48 -0300

    adicionada a feature de baixar audios

--- youtube-downloader/main.py
+++ youtube-downloader/main.py
@@ -1,5 +1,29 @@
+import os
+import subprocess
+
 from pytube import YouTube
 
-yt = YouTube('https://youtu.be/l2UDgpLz20M')
 
-print(yt.title)
+def download_audio(link: str):
+    yt = YouTube(link)
+    audio = yt.streams.get_audio_only()
+    path = audio.download( # type: ignore
+        output_path='/home/bruno/Downloads/'
+    ) 
+    convert_video_to_audio(path)
+
+
+def convert_video_to_audio(video_path: str):
+    name = video_path.replace('.mp4', '.mp3')
+    subprocess.run(['ffmpeg', '-i', video_path, name])
+    os.remove(video_path)
+
+
+def main():
+    link = 'https://www.youtube.com/watch?v=6CXJ61UaDhE'
+    # 'https://youtu.be/l2UDgpLz20M'
+    download_audio(link)
+
+
+if __name__ == '__main__':
+    main()
